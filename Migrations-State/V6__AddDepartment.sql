3afd8abe03de13c08a933f4a1d5be6560dc31d15
nadianur <nurmuez@gmail.com>
2025-09-22 20:12:26 +0200

    Add department table w departmentHead FK to instructor

---
+++ Migrations-State/V6__AddDepartment.sql
@@ -0,0 +1,10 @@
+﻿-- V6__AddDepartment.sql
+
+CREATE TABLE Department (
+    Id INT PRIMARY KEY IDENTITY(1,1),
+    Name NVARCHAR(200) NOT NULL,
+    Budget DECIMAL(18,2) NOT NULL,
+    StartDate DATE NOT NULL,
+    DepartmentHeadId INT NOT NULL,
+    FOREIGN KEY (DepartmentHeadId) REFERENCES Instructor(Id) ON DELETE CASCADE
+);
