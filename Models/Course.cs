b1e3709dd7ef8e85579b99cebf27597b9c057e66
Gülsüm <gulsumerd38@gmail.com>
2025-09-22 13:31:51 +0200

    EF code first: modify Course Credits from int to decimal

--- Models/Course.cs
+++ Models/Course.cs
@@ -10,7 +10,7 @@ namespace StudentManagement.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public int Credits { get; set; }
+        public decimal Credits { get; set; }
 
         public int InstructorId { get; set; }
         public Instructor Instructor { get; set; }
